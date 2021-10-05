f21a12dd394d53e90583e13edcb529d85a60a8e5
mlange806 <mlange806@gmail.com>
2021-10-05 02:08:56 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,8 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Edit"}
-
-@app.get("/newfeature")
-async def new_feature():
-    return {"wow!": "cool2"}
+    return {"message": "Hello World :)"}
