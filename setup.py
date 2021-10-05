da5b927fc3d8845de55c9b95a8f5502d2e080e49
mlange806 <mlange806@gmail.com>
2021-10-05 12:59:20 -0500

    add test structure

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="backend-demo", packages=find_packages())
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+import pytest
+import app.main
+
+def test_capital_case():
+    assert True
