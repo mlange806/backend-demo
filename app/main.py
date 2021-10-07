cf9562cdcc2839620a421d7f7af7c3a987593635
mlange806 <mlange806@gmail.com>
2021-10-07 01:27:54 -0500

    add security for endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import os
 import sqlalchemy
-from fastapi import FastAPI
+from app.security import *
+from datetime import timedelta
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 
 app = FastAPI()
 
@@ -22,8 +25,27 @@ db = sqlalchemy.create_engine(
 )
 
 
+@app.post("/token", response_model=Token)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
 @app.get("/")
-async def root():
+async def root(current_user: User = Depends(get_current_active_user)):
     shows = []
     with db.connect() as conn:
         shows = conn.execute("SELECT * FROM SHOWS").fetchall()
